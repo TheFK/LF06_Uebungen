35d39200eb216076505f927f91a84b81dc885ed4
Florian Klein <35765059+TheFK@users.noreply.github.com>
2021-03-17 11:07:50 +0100

    ip_toolbox stuff

--- fklein/ip_toolbox/js/toolbox.js
+++ fklein/ip_toolbox/js/toolbox.js
@@ -1,29 +1,37 @@
 var decimal_ip = [];
 var binary_ip = [];
-$("#ip_type").change(refreshInfos());
-$(".decimal_input").change(refreshInfos());
+var hexadecimal_ip = [];
+var ip_class = "";
+
+$(".decimal_input").change(function(){
+    refreshInfos();
+});
 
 function refreshInfos(){
-    if($("#ip_type").val() == "decimal"){
-        refreshDecimalCalculations();
-    }
-    else if($("#ip_type").val() == "binary"){
-        refreshBinaryCalculations();
-    }
+    refreshDecimalCalculations();
 
-    $("#decimal_ip").html(decimal_ip[0] + "." + decimal_ip[1] + "." + decimal_ip[2] + "." + 
-                            decimal_ip[3]);
-
-    $("#binary_ip").html(
-        binary_ip[0][0] + binary_ip[0][1] + binary_ip[0][2] + binary_ip[0][3] +
-        binary_ip[0][4] + binary_ip[0][5] + binary_ip[0][6] + binary_ip[0][7] + "." +
-        binary_ip[1][0] + binary_ip[1][1] + binary_ip[1][2] + binary_ip[1][3] +
-        binary_ip[1][4] + binary_ip[1][5] + binary_ip[1][6] + binary_ip[1][7] + "." +
-        binary_ip[2][0] + binary_ip[2][1] + binary_ip[2][2] + binary_ip[2][3] +
-        binary_ip[2][4] + binary_ip[2][5] + binary_ip[2][6] + binary_ip[2][7] + "." +
-        binary_ip[3][0] + binary_ip[3][1] + binary_ip[3][2] + binary_ip[3][3] +
-        binary_ip[3][4] + binary_ip[3][5] + binary_ip[3][6] + binary_ip[3][7]
+    $("#ip_decimal").html(
+        decimal_ip[0] + "." + 
+        decimal_ip[1] + "." + 
+        decimal_ip[2] + "." + 
+        decimal_ip[3]
     );
+
+    $("#ip_binary").html(
+        binary_ip[0] + "." +
+        binary_ip[1] + "." +
+        binary_ip[2] + "." +
+        binary_ip[3]
+    );
+
+    $("#ip_hexadecimal").html(
+        hexadecimal_ip[0] + "." +
+        hexadecimal_ip[1] + "." +
+        hexadecimal_ip[2] + "." +
+        hexadecimal_ip[3]
+    );
+
+    $("#ip_class").html(ip_class);
 }
 
 function refreshDecimalCalculations(){
@@ -36,73 +44,79 @@ function refreshDecimalCalculations(){
     binary_ip[1] = decimalToBinary(decimal_ip[1]);
     binary_ip[2] = decimalToBinary(decimal_ip[2]);
     binary_ip[3] = decimalToBinary(decimal_ip[3]);
+
+    hexadecimal_ip[0] = decimalToHexadecimal(decimal_ip[0]);
+    hexadecimal_ip[1] = decimalToHexadecimal(decimal_ip[1]);
+    hexadecimal_ip[2] = decimalToHexadecimal(decimal_ip[2]);
+    hexadecimal_ip[3] = decimalToHexadecimal(decimal_ip[3]);
+
+    ip_class = classOfCurrentIp();
 }
 
 function decimalToBinary(decimal){
-    var binary = [];
-    if(decimal >= 128){
-        binary[0] = 1;
-    }
-    else{
-        binary[0] = 0;
-    }
-    decimal -= 128;
-
-    if(decimal >= 64){
-        binary[1] = 1;
-    }
-    else{
-        binary[1] = 0;
-    }
-    decimal -= 64;
-
-    if(decimal >= 32){
-        binary[2] = 1;
-    }
-    else{
-        binary[2] = 0;
-    }
-    decimal -= 32;
+    var binary = "";
     
-    if(decimal >= 16){
-        binary[3] = 1;
+    var i;
+    for (i = 0; i < 8; i++) {
+        var bit = decimal % 2;
+        binary = "" + bit + binary;
+        decimal = decimal / 2;
+        if(bit == 1){
+            decimal -= 0.5;
+        }
     }
-    else{
-        binary[3] = 0;
-    }
-    decimal -= 16;
-
-    if(decimal >= 8){
-        binary[4] = 1;
-    }
-    else{
-        binary[4] = 0;
-    }
-    decimal -= 8;
-
-    if(decimal >= 4){
-        binary[5] = 1;
-    }
-    else{
-        binary[5] = 0;
-    }
-    decimal -= 4;
-
-    if(decimal >= 2){
-        binary[6] = 1;
-    }
-    else{
-        binary[6] = 0;
-    }
-    decimal -= 2;
-
-    if(decimal >= 1){
-        binary[7] = 1;
-    }
-    else{
-        binary[7] = 0;
-    }
-    decimal -= 1;
-
     return binary;
+}
+
+function decimalToHexadecimal(decimal){
+    var hexadecimal = "";
+    
+    var i;
+    for (i = 0; i < 2; i++) {
+        var hex = numberToHex(decimal % 16);
+        hexadecimal = "" + hex + hexadecimal;
+        decimal = decimal / 2;
+        decimal = Math.floor(decimal);
+    }
+    return hexadecimal;
+}
+
+function numberToHex(number){
+    switch (number){
+        case 10:
+            return 'A';
+        case 11:
+            return 'B';
+        case 12:
+            return 'C';
+        case 13:
+            return 'D';
+        case 14:
+            return 'E';
+        case 15:
+            return 'F';
+        default:
+            return number;
+    }
+}
+
+function classOfCurrentIp(){
+    if(decimal_ip[0] > 0 && decimal_ip[0] <= 127){
+        return 'A';
+    }
+    else if(decimal_ip[0] > 127 && decimal_ip[0] <= 191){
+        return 'B';
+    }
+    else if(decimal_ip[0] > 191 && decimal_ip[0] <= 223){
+        return 'C';
+    }
+    else if(decimal_ip[0] > 223 && decimal_ip[0] <= 239){
+        return 'D';
+    }
+    else if(decimal_ip[0] > 239 && decimal_ip[0] <= 255){
+        return 'E';
+    }
+    else{
+        return 'Unbekannte Klasse';
+    }
 }
